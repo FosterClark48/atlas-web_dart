bf8f0548bddbd998c91a979b34a08413c4d3e29d
FosterClark48 <fosterclark48@yahoo.com>
2024-02-17 15:01:02 -0600

    created a user class with a name property and method that returns a string with the name

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,10 @@
+// Create User class w/ prop 'name' and method showName that returns "Hello $name"
+class User {
+  // Propteries
+  String name = '';
+
+  // Method
+  String showName() {
+    return 'Hello $name';
+  }
+}
