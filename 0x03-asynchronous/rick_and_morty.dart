d2290504a8941b800e6e86525a9fceea3ce62fc1
FosterClark48 <fosterclark48@yahoo.com>
2024-02-19 00:09:10 -0600

    Created a function printRmCharacters() that prints all Rick and Morty Characters.

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,17 @@
+// Create a function printRmCharacters() that prints all Rick and Morty Characters.
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+Future<void> printRmCharacters() async {
+  try {
+    var uri = Uri.https('rickandmortyapi.com', '/api/character');
+    final response = await http.get(uri);
+    Map<dynamic, dynamic> data = convert.jsonDecode(response.body);
+    List<dynamic> characters = data['results'];
+    for (var character in characters) {
+      print(character['name']);
+    }
+  } catch (err) {
+    print('error caught: $err');
+  }
+}
